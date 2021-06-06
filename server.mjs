25cf5793636e6024f87d3b63eef7904b3c3a6b12
Сергей Луговых <lugowyh.serezha@yandex.ru>
2021-06-06 23:05:57 +0500

    added leaderboard creating from template

--- server.mjs
+++ server.mjs
@@ -1,7 +1,9 @@
 import * as path from "path";
 import express from "express";
+import hbs from "express-handlebars";
 import bodyParser from "body-parser";
 
+const rootDir = process.cwd();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -19,8 +21,36 @@ function addScore(user, score) {
     leaderboard[user] = oldScore + score;
 }
 
+function getSortedLeaderboard() {
+    return Object.entries(leaderboard)
+        .sort(([, a], [, b]) => b - a)
+        .map(([user, score], index) => ({
+            place: index + 1,
+            user: user,
+            score: score,
+        }));
+}
+
+app.set('view engine', 'hbs');
+app.engine(
+    'hbs',
+    hbs({
+        extname: 'hbs',
+        defaultView: 'default',
+        layoutsDir: path.join(rootDir, '/views'),
+        partialsDir: path.join(rootDir)
+    })
+);
+
 app.use(bodyParser.json());
-app.use(express.static(path.join(process.cwd(), 'client')));
+app.use(express.static(path.join(rootDir, 'client')));
+
+app.get('/leaderboard', (req, res) => {
+    res.render('leaderboard', {
+        layout: 'leaderboard',
+        userScores: getSortedLeaderboard()
+    });
+})
 
 app.post('/api/postScore', (req, res) => {
     console.log(req.body);
